c308ad308955853830d165da5be3a999eef5701a
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2020-09-28 15:35:47 +0300

    Add files via upload
    Python 3 auto youtube channel check

---
+++ autoy/autoy.py
@@ -0,0 +1,56 @@
+import pyautogui, time, pyperclip, keyboard
+
+pyautogui.countdown(5)
+
+fw = pyautogui.getActiveWindow()
+print(fw.title)
+print(fw.size)
+# fw.minimize()
+
+# pyautogui.mouseInfo()
+
+pyautogui.hotkey('winleft', 'r')
+time.sleep(1)
+pyautogui.write('firefox')
+time.sleep(1)
+pyautogui.hotkey('enter')
+time.sleep(1)
+pyautogui.write('https://www.youtube.com/')
+time.sleep(1)
+pyautogui.hotkey('enter')
+time.sleep(5)
+pyautogui.hotkey('tab')
+time.sleep(0.1)
+pyautogui.hotkey('tab')
+time.sleep(0.1)
+pyautogui.hotkey('tab')
+time.sleep(0.1)
+pyautogui.hotkey('tab')
+time.sleep(0.1)
+pyautogui.write('hololive')
+time.sleep(1)
+pyautogui.hotkey('enter')
+time.sleep(3)
+im = pyautogui.screenshot(r"scr\screenshot1.png")
+
+try:
+    x, y = pyautogui.locateCenterOnScreen('scr\scr1.png', confidence=0.9)
+    pyautogui.click(x, y)
+    time.sleep(5)
+    pyautogui.scroll(-800)
+
+    im1 = pyautogui.screenshot(r"scr\screenshot2.png")
+
+except:
+    print("Can`t find the image")
+
+try:
+    x, y = pyautogui.locateCenterOnScreen('scr\scr2.png', confidence=0.9)
+    pyautogui.click(x, y)
+    time.sleep(5)
+    pyautogui.scroll(-800)
+
+    im1 = pyautogui.screenshot(r"scr\screenshot2.png")
+
+except:
+    print("Can`t find the image")
