2cdd567d64798e28356aa305975abc457944c90e
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2020-12-12 18:05:15 +0300

    Add files via upload

--- ybot/ManuloMyot.py
+++ ybot/ManuloMyot.py
@@ -3,63 +3,86 @@ import pyautogui
 import pyperclip
 import time
 import multiprocessing
+import platform
+
+print(platform.python_implementation())
 
 time.sleep(5)  # Задержка начала выполнения в секундах
 
-
-def manulomyot():
+def manulomyot2054():
     s1 = " манул"
     s2 = " манула"
     s3 = " манулов"
-    num1 = 10
-    num2 = 1
-    num3 = 4
-    i = 1  # Счетчик манулов, можно ввести свое число для начала отсчета
+    s4 = " Мануломёт2054 подключается ... "
+    s5 = " Цель захвачена "
+    s6 = " Открываю огонь манулами на поражение "
+    num1 = 1
+    num2 = 4
+    i = 2792  # Счетчик манулов, можно ввести свое число для начала отсчета
+
+    pyperclip.copy(s4)
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(1)
+    pyautogui.press("Enter")
+    time.sleep(1)
+
+    pyperclip.copy(s5)
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(1)
+    pyautogui.press("Enter")
+    time.sleep(1)
+
+    pyperclip.copy(s6)
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(1)
+    pyautogui.press("Enter")
+    time.sleep(1)
 
     while True:
 
         if keyboard.is_pressed('Space'):  # Зажать пробел чтобы прервать цикл
             break
 
-        elif i == num2:
+        if i == num1:
             string = str(i) + s1
             pyperclip.copy(string)
             pyautogui.hotkey("ctrl", "v")
-            time.sleep(1)
+            # time.sleep(1)
             pyautogui.press("Enter")
-            time.sleep(1)
+            # time.sleep(1)
             i += 1
 
-        elif i <= num3:
+        if i <= num2:
             string = str(i) + s2
             pyperclip.copy(string)
             pyautogui.hotkey("ctrl", "v")
-            time.sleep(1)
+            # time.sleep(1)
             pyautogui.press("Enter")
-            time.sleep(1)
+            # time.sleep(1)
             i += 1
 
-        elif i // num1 == 0:
-            num1 += 10
-            num2 += 10
-            num3 += 10
-
-        elif i <= 999_999_999_999_999_999:
+        if i > num2:
             string = str(i) + s3
             pyperclip.copy(string)
             pyautogui.hotkey("ctrl", "v")
-            time.sleep(1)
+            # time.sleep(1)
             pyautogui.press("Enter")
-            time.sleep(1)
+            # time.sleep(1)
             i += 1
 
 
 if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=manulomyot())
-    p2 = multiprocessing.Process(target=manulomyot())
+    p1 = multiprocessing.Process(target=manulomyot2054())
+    p2 = multiprocessing.Process(target=manulomyot2054())
+    p3 = multiprocessing.Process(target=manulomyot2054())
+    p4 = multiprocessing.Process(target=manulomyot2054())
 
 p1.start()
 p2.start()
+p3.start()
+p4.start()
 
 p1.join()
 p2.join()
+p3.join()
+p4.join()
