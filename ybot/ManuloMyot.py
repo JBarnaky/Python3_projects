bcacfda50b064920f49f11ac0ff699bbfc8eaabe
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2021-01-18 21:39:09 +0300

    Add files via upload

--- ybot/ManuloMyot.py
+++ ybot/ManuloMyot.py
@@ -1,38 +1,31 @@
-import keyboard
 import pyautogui
 import pyperclip
 import time
 
-time.sleep(1)
+time.sleep(1)  # Задержка
 
 
-def autoInput(string):
+def autoInput(string):  # Функция автоввода
     pyperclip.copy(string)
     pyautogui.hotkey("ctrl", "v")
-    time.sleep(1)
+    time.sleep(1)  # Задержка ввода, можно менять значение
     pyautogui.press("Enter")
-    time.sleep(1)
+    time.sleep(1)  # Ожидание ввода, можно менять значение
 
 
 def manuloMyot228():
     manul_list = ['манул', 'манула', 'манулов', 'манулы']
-    manul_counter = 1
+    manul_counter = 1  # Счетчик манулов, можно менять значение
 
-    print("Прототип Призывателя Манулов версии 228 подключается ... ")
-    time.sleep(1)
+    print("Призыватель Манулов версии 228 подключается ... ")
+    time.sleep(1)  # Задержка
     input("Нажмите Enter чтобы продолжить...")
 
     for num in range(5):
         print(5 - num)
-        time.sleep(1)
+        time.sleep(1)  # Задержка
 
     while True:
-
-        if keyboard.is_pressed('Space'):
-            input("Нажмите Enter чтобы продолжить...")
-            for num in range(5):
-                print(5 - num)
-                time.sleep(1)
 
         if str(manul_counter) == str(11):
             string = str(manul_counter) + " " + manul_list[2]
