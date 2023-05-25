3bdc6dab7df1b6bd36102d58482c7c5c8edae6ca
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2023-05-25 17:24:48 +0300

    Update asmr.py

--- ybot/asmr.py
+++ ybot/asmr.py
@@ -3,59 +3,48 @@ import pyautogui
 import pyperclip
 import time
 
-# Задержка в секундах
-time.sleep(1)  
-
-
-def autoinput(string):  
-    """
-    Функция автоввода
-    """
+def autoinput(string):
     pyperclip.copy(string)
     time.sleep(0.1) 
     pyautogui.hotkey("ctrl", "v")
-    time.sleep(0.1) 
+    time.sleep(0.1)
     pyautogui.press("Enter")
-    time.sleep(10)  
-
+    time.sleep(5)
 
 def asmr():
-    manul_list = ['манул', 'манула', 'манулов', 'манулы']
-    manul_counter = 2293  
+    manul_tup = ('манул', 'манула', 'манулов', 'манулы')
+    manul_counter = 1  
 
-    # Подключение АСМР
-    print("АСМР - Автоматический Считатель Манулов Революционный версии 2754 подключается ... ")
-    time.sleep(1)  
+    print("АСМР - Автоматический Считатель Манулов Революционный версии 2754 подключается ... ")  
 
     input("Нажмите Enter чтобы продолжить...")
 
     for num in range(5):
-        print(5 - num)
-        time.sleep(1)  
+        print(5 - num) 
 
     while True:
         if keyboard.is_pressed('Space'):  
-            # Приостановка
             input("Нажмите Enter чтобы продолжить...")
             for num in range(5):
                 print(5 - num)
-                time.sleep(1)
 
-        # Обновление значения счетчика
         manul_str = str(manul_counter)
         manul_len = len(manul_str)
 
-        if manul_len == 2 and manul_str[1] == '1':
-            string = f"{manul_counter} {manul_list[2]}"
+        if manul_str == '11':
+            string = f"{manul_counter} {manul_tup[2]}"
         elif manul_str[-1] == '1':
-            string = f"{manul_counter} {manul_list[0]}"
-        elif manul_str[-1] in {'2', '3', '4'}:
-            string = f"{manul_counter} {manul_list[1]}"
+            string = f"{manul_counter} {manul_tup[0]}"
+        elif manul_str in {'12', '13', '14'}:
+            string = f"{manul_counter} {manul_tup[2]}"
+        elif '1' < manul_str[-1] <= '4':
+            string = f"{manul_counter} {manul_tup[1]}"
+        elif manul_str[-1] > '4' or manul_str[-1] == '0':
+            string = f"{manul_counter} {manul_tup[2]}"
         else:
-            string = f"{manul_counter} {manul_list[2]}"
+            string = f"{manul_counter} {manul_tup[2]}"
 
         autoinput(string)
         manul_counter += 1
-
 
 asmr()
