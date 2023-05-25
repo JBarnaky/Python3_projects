bdec8c2f285581ffd40aa3b0d07fd7b3f2f2b0fc
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2023-05-25 14:20:55 +0300

    Update calc.py

--- simplecalc/calc.py
+++ simplecalc/calc.py
@@ -4,29 +4,30 @@ while True:
     print("Enter, 'sub' to subtract two numbers")
     print("Enter, 'mult' to multiply two numbers")
     print("Enter, 'div' to divide two numbers")
+    print("Enter, 'quit' to exit the program")
     user_input = input(": ")
 
     if user_input == "quit":
+        print("Exiting program. Goodbye!")
         break
-    elif user_input == "add":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 + num2)
-        print("The answer is " + result)
-    elif user_input == "sub":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 - num2)
-        print("The answer is " + result)
-    elif user_input == "mult":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 * num2)
-        print("The answer is " + result)
-    elif user_input == "div":
-        num1 = float(input("Enter a number: "))
-        num2 = float(input("Enter another number: "))
-        result = str(num1 / num2)
-        print("The answer is " + result)
+    elif user_input in ["add", "sub", "mult", "div"]:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+
+        if user_input == "add":
+            result = num1 + num2
+        elif user_input == "sub":
+            result = num1 - num2
+        elif user_input == "mult":
+            result = num1 * num2
+        else:
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Error: division by zero")
+                continue
+
+        print(f"The result of {user_input} is {result}")
+        input("Press enter to continue")
     else:
-        print("Unknown input")
+        print("Unknown input. Please try again.") 
