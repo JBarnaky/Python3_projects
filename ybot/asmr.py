d0614612d09da5654c9f74e9e5d4c600d74aadf2
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2023-05-25 15:53:51 +0300

    Update asmr.py

--- ybot/asmr.py
+++ ybot/asmr.py
@@ -3,72 +3,59 @@ import pyautogui
 import pyperclip
 import time
 
-time.sleep(1)  # Задержка в секундах
+# Задержка в секундах
+time.sleep(1)  
 
 
-def autoinput(string):  # Функция автоввода
+def autoinput(string):  
+    """
+    Функция автоввода
+    """
     pyperclip.copy(string)
-    time.sleep(0.1)  # Задержка ввода, можно менять значение
+    time.sleep(0.1) 
     pyautogui.hotkey("ctrl", "v")
-    time.sleep(0.1)  # Задержка ввода, можно менять значение
+    time.sleep(0.1) 
     pyautogui.press("Enter")
-    time.sleep(10)  # Ожидание ввода, можно менять значение
+    time.sleep(10)  
 
 
 def asmr():
     manul_list = ['манул', 'манула', 'манулов', 'манулы']
-    manul_counter = 2293  # Счетчик манулов, можно менять значение
+    manul_counter = 2293  
 
-    print("АСМР - Автоматический Считатель Манулов Революционный версии 228 подключается ... ")
-    time.sleep(1)  # Задержка
+    # Подключение АСМР
+    print("АСМР - Автоматический Считатель Манулов Революционный версии 2754 подключается ... ")
+    time.sleep(1)  
+
     input("Нажмите Enter чтобы продолжить...")
 
     for num in range(5):
         print(5 - num)
-        time.sleep(1)  # Задержка
+        time.sleep(1)  
 
     while True:
-
-        if keyboard.is_pressed('Space'):  # Зажать Пробел чтобы приостановить
+        if keyboard.is_pressed('Space'):  
+            # Приостановка
             input("Нажмите Enter чтобы продолжить...")
             for num in range(5):
                 print(5 - num)
                 time.sleep(1)
 
-        if str(manul_counter) == str(11):
-            string = str(manul_counter) + " " + str(manul_list[2])
-            autoinput(string)
-            manul_counter += 1
+        # Обновление значения счетчика
+        manul_str = str(manul_counter)
+        manul_len = len(manul_str)
 
-        if str(manul_counter)[-1] == str(1):
-            string = str(manul_counter) + " " + str(manul_list[0])
-            autoinput(string)
-            manul_counter += 1
+        if manul_len == 2 and manul_str[1] == '1':
+            string = f"{manul_counter} {manul_list[2]}"
+        elif manul_str[-1] == '1':
+            string = f"{manul_counter} {manul_list[0]}"
+        elif manul_str[-1] in {'2', '3', '4'}:
+            string = f"{manul_counter} {manul_list[1]}"
+        else:
+            string = f"{manul_counter} {manul_list[2]}"
 
-        if str(manul_counter) == str(12):
-            string = str(manul_counter) + " " + str(manul_list[2])
-            autoinput(string)
-            manul_counter += 1
-
-        if str(manul_counter) == str(13):
-            string = str(manul_counter) + " " + str(manul_list[2])
-            autoinput(string)
-            manul_counter += 1
-
-        if str(manul_counter) == str(14):
-            string = str(manul_counter) + " " + str(manul_list[2])
-            autoinput(string)
-            manul_counter += 1
-
-        elif str(1) < str(manul_counter)[-1] <= str(4):
-            string = str(manul_counter) + " " + str(manul_list[1])
-            autoinput(string)
-            manul_counter += 1
-
-        elif str(manul_counter)[-1] > str(4) or str(manul_counter)[-1] == str(0):
-            string = str(manul_counter) + " " + str(manul_list[2])
-            autoinput(string)
-            manul_counter += 1
+        autoinput(string)
+        manul_counter += 1
 
 
 asmr()
