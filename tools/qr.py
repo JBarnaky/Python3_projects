5c37f40bf2d8c068ac40d574555b601ede128a83
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2024-08-06 18:22:59 +0300

    Update qr.py

--- tools/qr.py
+++ tools/qr.py
@@ -1,23 +1,50 @@
 import qrcode
 
+def generate_qr_code(data, filename):
+    """
+    Generate a QR code from the provided data and save it to a file.
+
+    Args:
+        data (str): The data to encode in the QR code.
+        filename (str): The filename to save the QR code image to.
+    """
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5  # Fix: typo in 'boeder'
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save(filename)
+
+# Example usage:
 data = 'https://www.linkedin.com/in/paul-yatskevich/'
-
-qr = qrcode.QRcode(version = 1, box_size = 10, boeder = 5)
-
-qr add_data(data)
-
-qr.make(fit=True)
-img = qr.make_image(fill_color = 'black', back_color = 'white')
-
-img.save('path')
+filename = 'linkedin_qr.png'
+generate_qr_code(data, filename)
 
 
 
-# from pyzbar.pyzbar import decode
-# from PIL import Image
 
-# img = Image.open('path')
 
-# result = decode(img)
+from pyzbar import pyzbar
+from PIL import Image
 
-# print(result)
+def decode_qr_code(filename):
+    """
+    Decode a QR code from an image file.
+
+    Args:
+        filename (str): The filename of the QR code image to decode.
+
+    Returns:
+        list: A list of decoded QR code data.
+    """
+    img = Image.open(filename)
+    decoded_data = pyzbar.decode(img)
+    return decoded_data
+
+# Example usage:
+filename = 'linkedin_qr.png'
+decoded_data = decode_qr_code(filename)
+print(decoded_data)
