151fa03e7239099594b3147a4e32ef0309ffe2d5
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2024-08-06 18:23:32 +0300

    Update qr.py

--- tools/qr.py
+++ tools/qr.py
@@ -11,7 +11,7 @@ def generate_qr_code(data, filename):
     qr = qrcode.QRCode(
         version=1,
         box_size=10,
-        border=5  # Fix: typo in 'boeder'
+        border=5
     )
     qr.add_data(data)
     qr.make(fit=True)
